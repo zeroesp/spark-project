d4b65b6bf2d574d16e3eeefa3a2916213ed7d341
zeroesp <zeroesp@gmail.com>
2018-05-03 22:37:52 +0900

    spark-sql 라이브러리 추가

--- src/main/scala/SimpleReadFile.scala
+++ src/main/scala/SimpleReadFile.scala
@@ -2,14 +2,21 @@ import org.apache.spark.{SparkContext, SparkConf}
 
 object SimpleReadFile {
   def main(args: Array[String]) : Unit = {
+    //windows hadoop conf
     System.setProperty("hadoop.home.dir", "C:\\winutils")
+
+    //Conf, Context
     val conf = new SparkConf().setAppName("SimpleReadFile").setMaster(args(0))
     val sc = new SparkContext(conf)
+
+    //logic -- start
     val filePath = "./pom.xml"
     val inputRDD = sc.textFile(filePath)
     val matchTerm = "spark"
     val numMatches = inputRDD.filter(_.contains(matchTerm)).count()
     println("%s lines in %s contains %s".format(numMatches, filePath, matchTerm))
+    //logic -- end
+
     System.exit(0)
   }
 }
