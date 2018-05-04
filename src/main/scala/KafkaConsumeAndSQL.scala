9e1a53d875641c26be63e7faca40b4632dd8eb87
zeroesp <zeroesp@gmail.com>
2018-05-04 10:51:08 +0900

    spark sql 수정 및 log level 설정 추가

--- src/main/scala/KafkaConsumeAndSQL.scala
+++ src/main/scala/KafkaConsumeAndSQL.scala
@@ -1,13 +1,15 @@
 import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.streaming.kafka010._
 import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
 import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
 import org.apache.spark.sql.SparkSession
+import org.apache.log4j.{Level, LogManager}
 
-case class Apt(year_month : String, reg_cd : String, reg_nm : String, dong_nm : String, apart : String, space : String, price : String, built_year : String, year : String, month : String, day : String, land_num : String, reg_cd2 : String, floor : String)
+//table schema
+case class Apt(year_month : String, reg_cd : String, reg_nm : String, dong_nm : String, apart : String, space : Float, price : Float, built_year : String, year : String, month : String, day : String, land_num : String, reg_cd2 : String, floor : String)
 
 object KafkaConsumeAndSQL {
   def main(args: Array[String]): Unit ={
@@ -15,8 +17,13 @@ object KafkaConsumeAndSQL {
     val sparkConf = new SparkConf().setAppName("KafkacConsumer").setMaster(args(0))
     val streamingContext = new StreamingContext(sparkConf, Seconds(10))
 
+    //sql context
     val sqlContext = SparkSession.builder().appName("sqltest").getOrCreate()
+    //DF transform
     import sqlContext.implicits._
+
+    //log level modify
+    LogManager.getRootLogger.setLevel(Level.WARN)
 
     //logic start
     val kafkaParams = Map[String, Object](
@@ -35,18 +42,24 @@ object KafkaConsumeAndSQL {
       Subscribe[String, String](topics, kafkaParams)
     )
 
-
     kafkaStream.foreachRDD(rdd => {
       println("* RDD size = " + rdd.count())
-      //r.foreach(s => println(s))
-      //sample Data : ConsumerRecord(topic = apart, partition = 8, offset = 9, CreateTime = 1525347287521, checksum = 1193147794, serialized key size = -1, serialized value size = 105, key = null, value = 201803|11110|서울특별시 종로구|창신동|창신쌍용2|79.87|46,200|1993|2018|3|11~20|703|11110|7)
-      rdd.foreach(s => println(s.value))
 
       rdd.map(s => s.value().split("\\|"))
-        .map(s => Apt(s(0), s(1), s(2), s(3), s(4), s(5), s(6), s(7), s(8), s(9), s(10), s(11), s(12), s(13)))
+        .map(s => Apt(s(0), s(1), s(2), s(3), s(4), s(5).toFloat, s(6).replace(",","").toFloat, s(7), s(8), s(9), s(10), s(11), s(12), s(13)))
         .toDF().createOrReplaceTempView("apt_table")
 
+      println("== apt_table")
       sqlContext.sql("select * from apt_table").collect().foreach(println)
+
+      println("== space ~80")
+      sqlContext.sql("select year_month, reg_nm, dong_nm, apart, space, price from apt_table where space < 80").collect().foreach(println)
+
+      println("== space 80~100")
+      sqlContext.sql("select year_month, reg_nm, dong_nm, apart, space, price from apt_table where space >= 80 and space < 100").collect().foreach(println)
+
+      println("== space 100~")
+      sqlContext.sql("select year_month, reg_nm, dong_nm, apart, space, price from apt_table where space >= 100").collect().foreach(println)
 
     })
     //logic end
